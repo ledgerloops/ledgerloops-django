60db9de6fe677b3891f2cb3bb78339d0229b826d
Michiel de Jong <michiel@unhosted.org>
2016-12-08 17:31:26 +0800

    Add form

--- ledger/urls.py
+++ ledger/urls.py
@@ -6,6 +6,8 @@ app_name = 'ledger'
 urlpatterns = [
     # ex: /ledger/
     url(r'^$', views.IndexView.as_view(), name='index'),
-    # ex: /ledger/bob/
+    # ex: /ledger/1/
     url(r'^(?P<pk>[0-9]+)/$', views.FriendView.as_view(), name='friend'),
+    # ex: /ledger/1/add
+    url(r'^(?P<pk>[0-9]+)/add$', views.AddView.as_view(), name='add'),
 ]
